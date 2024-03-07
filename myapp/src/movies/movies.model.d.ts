e236e23d77f4ec70bea23eddfc4ff6097600edb5
zeenat-abrar <zeenat@noesyssoftware.com>
2024-03-07 12:22:27 +0530

    routes

---
+++ myapp/src/movies/movies.model.d.ts
@@ -0,0 +1,11 @@
+
+export interface movieDTO {
+    id: number;
+    title: string;
+    poster: string;
+}
+
+export interface landingPageDTO {
+    inTheaters?: movieDTO[];
+    upcomingReleases?: movieDTO[];
+}
